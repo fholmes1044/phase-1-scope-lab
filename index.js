57aea4edbaa86e87f49bc607c404ffc15f36aace
+ <+>
2022-07-01 20:13:13 -0400

    commit tree 6615ec036749bbb96f7efcf906c1807cbfe1622e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob"; 
+const leastFavoriteCustomer = 'someone'
+function upperCaseCustomerName(){
+    return customerName = "BOB"
+}
+
+function setBestCustomer (){
+     bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'something else'
+    return leastFavoriteCustomer
+}
